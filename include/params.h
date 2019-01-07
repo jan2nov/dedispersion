346f93ab6b2b9ed4fc1f1953027fdab7ad9e197a
Jan Novotny <jan.novotny@fpf.slu.cz>
2019-01-07 12:02:43 +0000

    Init of new clean dedispersion cpu version

---
+++ include/params.h
@@ -0,0 +1,5 @@
+#define NSAMPLES  194
+#define RUNS 1
+#define IB  1*NSAMPLES
+#define IC 64
+#define IJ 128
---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+#include <math.h>
+#include "params.h"
+#include <malloc.h>
+
+#include <immintrin.h>
+
+#define ALIGNSIZE 64
+#define PLATFORM 0
+
+#define SIMDWIDTH 16
+
+int main(int argc, char *argv[])
+{
+	printf("That's All folks!\n");
+	return 0;
+}
