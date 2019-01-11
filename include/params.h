798a23a0b71623da1475305d8967d87f1d2923b8
Jan Novotny <jan.novotny@fpf.slu.cz>
2019-01-11 17:17:26 +0000

    changing the signal length to 5s

--- include/params.h
+++ include/params.h
@@ -1,11 +1,11 @@
 // dedispersion settings
 #define NSAMPLES  176
 #define RUNS 5
-#define DIVINDM 50
+#define DIVINDM 125
 #define DIVINCHAN 64
 
 //transpose settings
 #define TR_BLOCK 32
 
 //fake signal settings
-#define SIGNAL_TIME 10
+#define SIGNAL_TIME 5
